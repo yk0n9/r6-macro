75324b76b10fbd9b7ee8b452443af3607706ad67
ykong1337 <ykonghao@icloud.com>
2024-01-09 21:53:46 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,101 @@
+use std::mem::{size_of, zeroed};
+use std::thread;
+use std::time::Duration;
+
+use windows::Win32::UI::Input::KeyboardAndMouse::{
+    GetAsyncKeyState, INPUT, INPUT_0, INPUT_MOUSE, MOUSEEVENTF_MOVE, MOUSEINPUT, SendInput,
+    VIRTUAL_KEY, VK_LBUTTON, VK_RBUTTON,
+};
+
+#[derive(Debug, Copy, Clone, Default, Eq, PartialEq)]
+enum Aim {
+    Left,
+    Right,
+    #[default]
+    None,
+}
+
+#[derive(Debug, Copy, Clone, Default)]
+struct State {
+    add: bool,
+    sub: bool,
+}
+
+#[derive(Debug, Copy, Clone, Default)]
+struct Level {
+    level: i32,
+    aim: Aim,
+    state: State,
+}
+
+fn main() {
+    let mut level = Level {
+        level: 9,
+        ..Default::default()
+    };
+    println!("level: {}", level.level);
+    loop {
+        if !is_pressed(VK_RBUTTON) && !is_pressed(VK_LBUTTON) {
+            level.aim = Aim::None;
+        }
+
+        let sub = is_pressed(VIRTUAL_KEY(189)) || is_pressed(VIRTUAL_KEY(109));
+        if !sub {
+            level.state.sub = false
+        }
+        if sub != level.state.sub && level.level > 1 {
+            level.state.sub = sub;
+            level.level -= 1;
+            println!("level: {}", level.level);
+        }
+        let add = is_pressed(VIRTUAL_KEY(187)) || is_pressed(VIRTUAL_KEY(107));
+        if !add {
+            level.state.add = false
+        }
+        if add != level.state.add {
+            level.state.add = add;
+            level.level += 1;
+            println!("level: {}", level.level);
+        }
+
+        match level.aim {
+            Aim::Right => {
+                if is_pressed(VK_RBUTTON) && is_pressed(VK_LBUTTON) {
+                    mouse_down(level.level);
+                }
+            }
+            Aim::None => {
+                if is_pressed(VK_LBUTTON) {
+                    level.aim = Aim::Left;
+                } else if is_pressed(VK_RBUTTON) {
+                    level.aim = Aim::Right
+                }
+            }
+            _ => {}
+        }
+    }
+}
+
+static mut I: INPUT = INPUT {
+    r#type: INPUT_MOUSE,
+    Anonymous: INPUT_0 {
+        mi: MOUSEINPUT {
+            dwFlags: MOUSEEVENTF_MOVE,
+            ..unsafe { zeroed() }
+        },
+    },
+};
+
+#[inline]
+fn mouse_down(val: i32) {
+    unsafe {
+        I.Anonymous.mi.dy = val;
+        SendInput(&[I], size_of::<INPUT>() as i32);
+    }
+    thread::sleep(Duration::from_millis(10));
+}
+
+#[inline]
+fn is_pressed(vk: VIRTUAL_KEY) -> bool {
+    unsafe { GetAsyncKeyState(vk.0 as i32) as u32 >> 31 == 1 }
+}
